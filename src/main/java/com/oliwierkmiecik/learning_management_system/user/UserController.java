2a52280ffc0230c49020d4f59a6a842553972488
Oliwier Kmiecik <oliwier.kmiecik@wp.pl>
2025-03-16 19:20:27 +0000

    Added method descriptions for swagger documentation

--- src/main/java/com/oliwierkmiecik/learning_management_system/user/UserController.java
+++ src/main/java/com/oliwierkmiecik/learning_management_system/user/UserController.java
@@ -2,6 +2,9 @@ package com.oliwierkmiecik.learning_management_system.user;
 
 import com.oliwierkmiecik.learning_management_system.user.dto.UserCreateDTO;
 import com.oliwierkmiecik.learning_management_system.user.dto.UserReadDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -19,16 +22,34 @@ public class UserController {
         this.userMapper = userMapper;
     }
 
+
+    @Operation(description = "Downloads all users",
+    responses = {
+            @ApiResponse(responseCode = "200", description = "Users downloaded successfully")
+    })
     @GetMapping("users")
     public ResponseEntity<List<UserReadDTO>> getAllUsers() {
         return ResponseEntity.ok(userMapper.entityListToReadDTOList(userService.findAllUsers()));
     }
 
+    @Operation(description = "Downloads user by its id",
+    responses = {
+            @ApiResponse(responseCode = "200", description = "User downloaded successfully"),
+            @ApiResponse(responseCode = "404", description = "User for this id not found"),
+            @ApiResponse(responseCode = "400", description = "Most likely stated id was not a number")
+    }, parameters = {
+            @Parameter(name = "id", description = "Id for the user that is meant to be downloaded.")
+    })
     @GetMapping("users/{id}")
     public ResponseEntity<UserReadDTO> getUserById(@PathVariable Integer id) {
         return ResponseEntity.ok(userMapper.userToUserReadDTO(userService.findUserById(id)));
     }
 
+    @Operation(description = "Creates a new user in the database",
+    responses = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "403", description = "Not all fields were filled")
+    })
     @PostMapping("users")
     public ResponseEntity<UserReadDTO> createUser(@RequestBody UserCreateDTO userCreateDTO) {
         User newUser = userService.saveUser(userCreateDTO);
@@ -43,16 +64,19 @@ public class UserController {
         return ResponseEntity.created(location).body(userReadDTO);
     }
 
+    @Operation(description = "Updates the whole user entity in the database")
     @PutMapping("users/{id}")
     public ResponseEntity<UserReadDTO> updateUser(@PathVariable Integer id, @RequestBody UserCreateDTO updates) {
         return ResponseEntity.ok(userMapper.userToUserReadDTO(userService.updateUser(id, updates, true)));
     }
 
+    @Operation(description = "Partially updates user entity in the database")
     @PatchMapping("users/{id}")
     public ResponseEntity<UserReadDTO> partiallyUpdateUser(@PathVariable Integer id, @RequestBody UserCreateDTO updates) {
         return ResponseEntity.ok(userMapper.userToUserReadDTO(userService.updateUser(id, updates, false)));
     }
 
+    @Operation(description = "Deletes user from database")
     @DeleteMapping("users/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
